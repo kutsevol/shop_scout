aa50065f000c6dd6f53d3f220fdadec4321e75d6
Oleksandra <bereznai.oleksandra@gmail.com>
2025-08-08 13:59:02 +0200

    add mock countries endpoint change name fun get_countries, version of project change name fun get_countries, version of project remove pandas

--- backend/src/static.py
+++ backend/src/static.py
@@ -1,3 +1,39 @@
+from dto.country import Country
 from dto.shop import Shop
 
 shops = [Shop(id=1, name="Auchan"), Shop(id=2, name="Metro"), Shop(id=3, name="Silpo")]
+
+countries = [
+    Country(
+        id=1,
+        name="Ukraine",
+        code="UA",
+        emoji="🇺🇦",
+        unicode="U+1F1FA U+1F1E6",
+        image="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/UA.svg",
+    ),
+    Country(
+        id=2,
+        name="France",
+        code="FR",
+        emoji="🇫🇷",
+        unicode="U+1F1EB U+1F1F7",
+        image="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/FR.svg",
+    ),
+    Country(
+        id=3,
+        name="Spain",
+        code="ES",
+        emoji="🇪🇸",
+        unicode="U+1F1EA U+1F1F8",
+        image="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/ES.svg",
+    ),
+    Country(
+        id=4,
+        name="Germany",
+        code="DE",
+        emoji="🇩🇪",
+        unicode="U+1F1E9 U+1F1EA",
+        image="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/DE.svg",
+    ),
+]
