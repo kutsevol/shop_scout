0ba1c4324ace24030ab3616e06d23e5032e65cb3
anastasiiafedurko <your-email@example.com>
2025-08-13 11:26:36 +0200

    got countries from BE

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import countries, index, shops
 from core.logging_config import log_config
--- frontend/src/services/countries.ts
+++ frontend/src/services/countries.ts
@@ -1,11 +1,16 @@
 import type { Country } from "../types/country";
-import { getData } from "../utils/httpClient";
+import { client } from "../utils/httpClient";
+// import { getData } from "../utils/httpClient";
 
-export async function getCountries(): Promise<Country[]> {
-  try {
-    return await getData<Country[]>("/countries.json");
-  } catch (error) {
-    console.error("Fetching countries failed: ", error);
-    throw new Error("Failed to load country list. Please try again later.");
-  }
-}
+// export async function getCountries(): Promise<Country[]> {
+//   try {
+//     return await getData<Country[]>("/countries");
+//   } catch (error) {
+//     console.error("Fetching countries failed: ", error);
+//     throw new Error("Failed to load country list. Please try again later.");
+//   }
+// }
+
+export const getCountries = () => {
+  return client.get<Country[]>(`/countries/`);
+};
