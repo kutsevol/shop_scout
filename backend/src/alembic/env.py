1d54921f83fe81ba7803aa3940283753a7de5960
Oleksandra <bereznai.oleksandra@gmail.com>
2025-12-10 21:16:05 +0100

    add migrations alembic, fix database_url

---
+++ backend/src/alembic/env.py
@@ -0,0 +1,74 @@
+import asyncio
+import sys
+from logging.config import fileConfig
+from pathlib import Path
+
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlmodel import SQLModel
+
+from alembic import context  # type: ignore
+from core.config import settings
+
+# ─────────────────────────────────────────────
+# Fix PATH so Alembic can import core.*
+# ─────────────────────────────────────────────
+BASE_DIR = Path(__file__).resolve().parents[1]
+sys.path.append(str(BASE_DIR))
+
+
+# Alembic Config
+config = context.config
+
+fileConfig(config.config_file_name)
+
+# Import all models automatically
+target_metadata = SQLModel.metadata
+
+# Set DB URL dynamically from settings
+config.set_main_option("sqlalchemy.url", settings.database_url_sync)
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in offline mode."""
+
+    url = settings.database_url_sync
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    """Sync migration logic."""
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Run migrations in online (async) mode."""
+
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
